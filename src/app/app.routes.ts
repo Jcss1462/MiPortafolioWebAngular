967d8f161847d1604830cff8e677a7f03179db27
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-06-23 21:54:42 -0500

    inicio de configuracion del header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainPageComponent } from './domains/main/pages/main-page/main-page.component';
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: MainPageComponent
+    },
+];
