2af07f2a4db9b68cc2181a2e5366b0003f2baa31
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-06-26 20:16:47 -0500

    Separacion por componentes

--- src/app/domains/main/pages/main-page/main-page.component.ts
+++ src/app/domains/main/pages/main-page/main-page.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { AboutMeComponent } from '../../components/about-me/about-me.component';
+import { ExperienceComponent } from '../../components/experience/experience.component';
+
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, AboutMeComponent, ExperienceComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-
-
-  
-
 
 
 }
