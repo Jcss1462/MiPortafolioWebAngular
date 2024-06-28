b35213b63c3ef3b4b8159d62ca01a15d3eb5916b
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-06-27 19:13:49 -0500

    proyectos destacados finalizado

--- src/app/domains/main/components/projects/projects.component.ts
+++ src/app/domains/main/components/projects/projects.component.ts
@@ -33,7 +33,58 @@ export class ProjectsComponent {
         url:"https://store-1wemzdrvj-juan-camilo-salazar-sernas-projects.vercel.app/"
       }
     ]
+    },
+
+    {
+      Title:"Tareas Api",
+      Description: "Api Creada con .Net y EntityFramework para la gestión de tareas",
+      mainImage: "assets/apiImage.jpg",
+      icons:[{
+        TecnolgyName:".Net",
+        Img:"assets/NET_Core_Logo.png"
+      }],
+      links:[{
+        linkName:"GitHub",
+        class:"bi bi-github",
+        url:"https://github.com/Jcss1462/ApisConPuntoNet"
+      }
+    ]
+    },
+    {
+      Title:"La mano de buzz",
+      Description: "Composición creada con blender",
+      mainImage: "assets/buzzHand.PNG",
+      icons:[{
+        TecnolgyName:"Blender",
+        Img:"assets/blenderLogo.png"
+      }],
+      links:[{
+        linkName:"Instagram",
+        class:"bi bi-instagram",
+        url:"https://www.instagram.com/p/CihIijJO_ST/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      }
+    ]
+    },
+    {
+      Title:"LowPolly En el Espacio",
+      Description: "Composición creada con blender y Unity",
+      mainImage: "assets/lowPollyEnElEspacio.PNG",
+      icons:[{
+        TecnolgyName:"Blender",
+        Img:"assets/blenderLogo.png"
+      },{
+        TecnolgyName:"Unity",
+        Img:"assets/unityLogo.png"
+      }],
+      links:[{
+        linkName:"Instagram",
+        class:"bi bi-instagram",
+        url:"https://www.instagram.com/p/CkVBCiks7qc/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+      }
+    ]
     }
+
+
   ]
 
 }
