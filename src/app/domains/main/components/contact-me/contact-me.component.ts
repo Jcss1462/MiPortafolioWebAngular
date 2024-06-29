6200331cf66b7be583e7edcbe3fe0aff55a34cba
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-06-29 13:15:45 -0500

    se añade spinner de carga para el envio del formulario

--- src/app/domains/main/components/contact-me/contact-me.component.ts
+++ src/app/domains/main/components/contact-me/contact-me.component.ts
@@ -1,10 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
-
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { SendMailService } from '../../../shared/services/send-mail.service';
-
 import { MailBody } from '../../../shared/models/mailBody.model';
+import { SpinnerService } from '../../../shared/services/spinner.service';
 
 @Component({
   selector: 'app-contact-me',
@@ -17,6 +16,7 @@ import { MailBody } from '../../../shared/models/mailBody.model';
 export class ContactMeComponent {
 
   private sendMailService = inject(SendMailService);
+  private spinnerService = inject(SpinnerService);
   contactForm!: FormGroup;
 
 
@@ -37,22 +37,26 @@ export class ContactMeComponent {
 
     if (this.contactForm.valid) {
 
-      let mailBody:MailBody={
-        to:"jcss1462@gmail.com",
-        subject:this.contactForm.get("asunto")?.value,
-        text:this.contactForm.get("nombre")?.value+" Te envió el siguiente mensaje:\n\n"+this.contactForm.get("mensaje")?.value+"\n\nContactalo a su correo: "+this.contactForm.get("email")?.value
+      let mailBody: MailBody = {
+        to: "jcss1462@gmail.com",
+        subject: this.contactForm.get("asunto")?.value,
+        text: this.contactForm.get("nombre")?.value + " Te envió el siguiente mensaje:\n\n" + this.contactForm.get("mensaje")?.value + "\n\nContactalo a su correo: " + this.contactForm.get("email")?.value
       }
 
+      this.spinnerService.showSpinner.update(() => true);
       this.sendMailService.addProduct(mailBody)
         .subscribe({
           next: (data) => {
             this.toastr.success("Mensaje enviado con éxito");
             this.contactForm.reset();
+            this.spinnerService.showSpinner.update(() => false);
           },
           error: (err) => {
             this.toastr.error("Ocurrió un error al enviar el correo, intenta más tarde");
+            this.spinnerService.showSpinner.update(() => false);
           }
         })
+      
 
     } else {
       this.toastr.error("Llena todos los campos obligatorios");
--- src/app/domains/main/pages/main-page/main-page.component.ts
+++ src/app/domains/main/pages/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { AboutMeComponent } from '../../components/about-me/about-me.component';
 import { ExperienceComponent } from '../../components/experience/experience.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { StackComponent } from '../../components/stack/stack.component';
 import { ContactMeComponent } from '../../components/contact-me/contact-me.component';
+import { SpinnerService } from '../../../shared/services/spinner.service';
 
 
 @Component({
@@ -15,6 +16,6 @@ import { ContactMeComponent } from '../../components/contact-me/contact-me.compo
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-
-
+  private  spinnerService= inject(SpinnerService);
+  showSpinner=this.spinnerService.showSpinner;
 }
