0650db9f8f4b603ae43ede0898d99d4427504ac2
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-06-29 18:15:52 -0500

    ajauste visual del header y se añade funcionalidad a los botones del header

--- src/app/domains/main/components/header/header.component.ts
+++ src/app/domains/main/components/header/header.component.ts
@@ -9,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  scrollToSection(sectionId: string) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  downloadPDF() {
+    const link = document.createElement('a');
+    link.href = 'assets/esp_cv_JuanCamiloSalazarSerna_2024.pdf';  // Asegúrate de que la ruta sea correcta
+    link.download = 'esp_cv_JuanCamiloSalazarSerna_2024.pdf'; // Nombre del archivo que se descargará
+    link.click();
+  }
+
 }
