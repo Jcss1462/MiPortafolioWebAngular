d872cbfac7542f4c9e0d4659fc4bf922f688bdb5
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2025-01-07 22:17:23 -0500

    Se hacen ajustes al servicio de correro, para funcionar con la nueva ip y qeu se pueda configurar la ruta del api desde las variables de entorno

--- src/app/domains/shared/services/send-mail.service.ts
+++ src/app/domains/shared/services/send-mail.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { MailBody } from '../models/mailBody.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,6 @@ export class SendMailService {
     const headers = new HttpHeaders({
       "Content-Type" : 'application/json; charset=utf-8'
     });
-    return this.http.post( 'https://servicio-correo-node-efb3ac491db9.herokuapp.com/send-email', mailBody,{ responseType: 'text'});
+    return this.http.post( environment.mailApi, mailBody,{ responseType: 'text'});
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,6 @@ export const environment = {
     messagingSenderId: "71796819636",
     appId: "1:71796819636:web:f02cc82effadefcca4f9e1",
     measurementId: "G-6W6N55VT2W"
-  }
+  },
+  mailApi:"https://54.221.194.158:8443/send-email"
 };
